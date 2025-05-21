8f74c363667f807cb4bcb0e700191001915cd954
mattbeutel <matthewbeutel@ufl.edu>
2025-05-21 17:31:32 -0400

    Exercise 7 complete

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split('');
+    for (let i = 0; i < (arr.length - 1)/2; i++) {
+        temp = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = temp;
+    }
+    return arr.join('');
 };
 
 // Do not edit below this line
