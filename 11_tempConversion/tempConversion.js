33f162395628c42caba845496441c396d550b3f2
mattbeutel <matthewbeutel@ufl.edu>
2025-05-21 17:56:57 -0400

    Exercise 11 complete

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let res = (temp-32)*(5/9);
+  res *= 10;
+  res -= (res%1);
+  if (res % 10 >= 5) {
+    res += 1;
+  }
+  res /= 10;
+  return res;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let res = temp*(9/5)+32;
+  res *= 10;
+  res -= (res%1);
+  if (res % 10 >= 5) {
+    res += 1;
+  }
+  res /= 10;
+  return res;
 };
 
 // Do not edit below this line
