677d8dcd3247433cbc538b3d3ea72461a924e799
mattbeutel <matthewbeutel@ufl.edu>
2025-05-21 17:26:15 -0400

    Exercise 6 complete

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, amount) {
+    if (amount < 0) {
+        return "ERROR";
+    }
+    let res = "";
+    for (let i = 0; i < amount; i++) {
+        res += string;
+    }
+    return res;
 };
 
 // Do not edit below this line
